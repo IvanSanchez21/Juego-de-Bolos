20ce2e84cbdc88af95778a3a3ecae348b451099e
Ivàn Sànchez <49045265+ivanx21@users.noreply.github.com>
2022-02-02 06:59:31 -0500

    Initial commit

---
+++ Assets/UdacityVR/Scripts/MisScripts/CambiarEscena.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiarEscena : MonoBehaviour {
+
+	public void IrAEscena(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void salir(){
+        Application.Quit();
+    }
+}
